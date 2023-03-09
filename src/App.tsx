148bd77589f83e584ce1352bc43f24a697739d82
Krystian Kowalski <krycha1515@gmail.com>
2023-03-09 21:08:14 +0100

    Create Header component with hamburger menu

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,12 @@ import {
 	LeaderboardsPage,
 	SummonerPage,
 } from "./pages";
+import Header from "./components/Header/Header";
 
 function App() {
 	return (
 		<Router>
+			<Header />
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/summoner" element={<SummonerPage />} />
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect, useRef } from "react";
+import { NavLink, Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import navLinks from "../../utils/nav-links";
+import styles from "./Header.module.scss";
+
+const Header = () => {
+	const [showLinks, setShowLinks] = useState(false);
+	const linksContainerRef = useRef<HTMLDivElement>(null!);
+	const linksRef = useRef<HTMLElement>(null!);
+
+	useEffect(() => {
+		const linksHeight = linksRef.current.getBoundingClientRect().height;
+		if (showLinks) {
+			linksContainerRef.current.style.height = `${linksHeight}px`;
+		} else {
+			linksContainerRef.current.style.height = `0px`;
+		}
+	}, [showLinks]);
+
+	return (
+		<header className={styles.header}>
+			<div className={styles.header__center}>
+				<div className={styles.header__nav}>
+					<Link to="/">
+						<h1>LoL Profile</h1>
+					</Link>
+
+					<button
+						className={styles.header__hamburger}
+						onClick={() => setShowLinks(!showLinks)}
+					>
+						<FaBars />
+					</button>
+				</div>
+
+				<div className={styles.header__linksContainer} ref={linksContainerRef}>
+					<nav className={styles.header__links} ref={linksRef}>
+						{navLinks.map((link) => (
+							<NavLink
+								key={link.id}
+								to={link.url}
+								onClick={() => setShowLinks(false)}
+							>
+								{link.text}
+							</NavLink>
+						))}
+					</nav>
+				</div>
+			</div>
+		</header>
+	);
+};
+export default Header;
